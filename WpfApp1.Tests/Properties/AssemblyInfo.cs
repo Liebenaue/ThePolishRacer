b7e652425d2f7b072c305b4842867d29c2eb59ec
Liebenaue <maxu7331@interia.pl>
2025-06-28 20:32:43 +0200

    feat dodano testy jednostkowe do CI

---
+++ WpfApp1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WpfApp1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WpfApp1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("879d6b9f-5187-4d93-a660-823321c2c94d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WpfApp1.Tests/SampleTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WpfApp1.Tests
+{
+    [TestClass]
+    public class SampleTests
+    {
+        [TestMethod]
+        public void AlwaysPasses()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
